0d1bd3fe80fe3af7ee2e8b60036a5dfb946a9b42
Parkinson-Drew <parkidr@gmail.com>
2020-10-10 16:05:58 -0700

    revert repo sentence appended.

--- src/parkinson/Hello.java
+++ src/parkinson/Hello.java
@@ -12,5 +12,6 @@ public class Hello {
         System.out.println("Hello Drew, my name is Frank.");
         System.out.println("Nice to meet you Frank.");
         System.out.println("Thanks for updating my repo.");
+        System.out.println("Going to revert changes on my repo.");
     }
 }
