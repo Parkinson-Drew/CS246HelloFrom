768004dc2de8c62d8689c06aa255a934a33d2543
Parkinson-Drew <parkidr@gmail.com>
2020-10-10 12:21:55 -0700

    Initial commit using Drews account.

---
+++ src/parkinson/Hello.java
@@ -0,0 +1,13 @@
+package parkinson;
+
+public class Hello {
+
+    /**
+     * Prints greeting to a user
+     * @param args
+     */
+    public  static  void main(String[] args){
+        Hello hello = new Hello();
+        System.out.println("Hello, my name is Drew");
+    }
+}
