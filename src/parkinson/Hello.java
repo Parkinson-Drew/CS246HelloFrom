145217ae2f7fd69046bf6831349ccca4c9bfad1c
Parkinson-Drew <parkidr@gmail.com>
2020-10-10 16:02:52 -0700

    added another sentence

--- src/parkinson/Hello.java
+++ src/parkinson/Hello.java
@@ -4,11 +4,12 @@ public class Hello {
 
     /**
      * Prints greeting to a user
-     * @param args
+     * @param args passes arguments
      */
     public  static  void main(String[] args){
         Hello hello = new Hello();
         System.out.println("Hello, my name is Drew");
         System.out.println("Hello Drew, my name is Frank.");
+        System.out.println("Nice to meet you Frank.");
     }
 }
